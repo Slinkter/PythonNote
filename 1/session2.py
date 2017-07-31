63b4c963d5256eca5c353263c82cf2298ec7c7f2
Slinkter <luis.j.cueva@gmail.com>
2017-07-30 19:27:13 -0500

    session 2 : fin

--- 1/session2.py
+++ 1/session2.py
@@ -1,7 +1,69 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jul 30 18:45:37 2017
+class MiClase(object):
+    """
+    Mi clase doc
+    """
+    class_prop = "Propiedad de clase"
 
-@author: John
-"""
+    def __init__(self, nombre=None, verbose=False) :
+        """
+        Documentacion de constructor
+        """
+        self.data = []
+        self._private_data = []
+        self._nombre = nombre
+    
+    def __del__(self):
+        pass
+    
+    def __add__(self, a):
+        pass
+    
+    def __getitem__(self, key):
+        pass
+    
+    def function(self):
+        """
+        Documentacion de methodo
+        """
+        self._privada()
+        print("Este mensaje se imprime desde la clase")
+    
+    def _privada(self):
+        """
+        Documentacion de methodo
+        """
+        print("Este mensaje se imprime desde la clase")
+        
+    @staticmethod
+    def metodo_estatico():
+        print("Statico, no tiene acceso a 'self'")
+    
+    @classmethod
+    def verboso(cls):
+        print("Metodo de clase")
+        a = cls(verbose=False)
+        del a.data
+    
+    @property
+    def nombre(self):
+        print("Calling nombre")
+        return self._nombre
+    
+    @nombre.setter
+    def nombre(self, a):
+        self._nombre = a
+        print("Calling setter")
+    
+    def algo(self):
+        """
+        doc de lo que hago
+        """
+        raise NotImplementedError()
+        
+        
+mi_lista = [1,2,3,"hola",5]
+mi_lista.count("hola")
+mi_lista.append("Nuevo")
+print(mi_lista)
+
 
